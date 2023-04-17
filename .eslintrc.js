aefb5cb2833980cccb18f30078cb90c45c5421d0
NamTV22 <vietnamtudonghoa98@gmail.com>
2023-04-17 10:14:00 +0700

    basic folder structure

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  //   parser: "@typescript-eslint/parser",
+  //   plugins: ["@typescript-eslint"],
+  //   extends: [
+  //     "eslint:recommended",
+  //     "plugin:react/recommended",
+  //     "plugin:react-hooks/recommended",
+  //     "plugin:jsx-a11y/recommended",
+  //     "plugin:@typescript-eslint/recommended",
+  //   ],
+  //   env: {
+  //     es6: true,
+  //     browser: true,
+  //     jest: true,
+  //     node: true,
+  //   },
+  //   rules: {
+  //     "react/jsx-sort-props": [
+  //       "error",
+  //       {
+  //         ignoreCase: true,
+  //         reservedFirst: true,
+  //       },
+  //     ],
+  //   },
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+
+  env: {
+    es6: true,
+  },
+};
---
+++ src/utils/CustomCryptoJS.utils.ts
@@ -0,0 +1,49 @@
+import CryptoJS from "crypto-js";
+
+const SECRET_KEY = process.env.REACT_APP_SECRET_KEY || "";
+
+const encryptData = (name: string, data: any) => {
+  const encrypted = CryptoJS.AES.encrypt(
+    JSON.stringify(data),
+    SECRET_KEY
+  ).toString();
+  localStorage.setItem(name, encrypted);
+};
+
+const decryptData = (name: string) => {
+  const encrypted = localStorage.getItem(name);
+
+  if (!encrypted) {
+    return null;
+  }
+
+  const decrypted = CryptoJS.AES.decrypt(encrypted, SECRET_KEY).toString(
+    CryptoJS.enc.Utf8
+  );
+
+  return JSON.parse(decrypted);
+};
+
+const sessionEncryptData = (name: string, data: any) => {
+  const encrypted = CryptoJS.AES.encrypt(
+    JSON.stringify(data),
+    SECRET_KEY
+  ).toString();
+  sessionStorage.setItem(name, encrypted);
+};
+
+const sessionDecryptData = (name: string) => {
+  const encrypted = sessionStorage.getItem(name);
+
+  if (!encrypted) {
+    return null;
+  }
+
+  const decrypted = CryptoJS.AES.decrypt(encrypted, SECRET_KEY).toString(
+    CryptoJS.enc.Utf8
+  );
+
+  return JSON.parse(decrypted);
+};
+
+export { encryptData, decryptData, sessionEncryptData, sessionDecryptData };
